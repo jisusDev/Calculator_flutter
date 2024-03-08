b67cb1021ad4a13e7ed29c548bb804696f7c03c7
Francisco.V98 <frncsico@gmail.com>
2024-03-08 14:05:40 -0400

    Create skeleton screen, I have to switch to real widgets

--- lib/screens/calculator_screen/calculator_screen.dart
+++ lib/screens/calculator_screen/calculator_screen.dart
@@ -8,8 +8,100 @@ class CalculatorScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Center(
-        child: Text('Hello World!'),
+      backgroundColor: Colors.black,
+      body: _Body(),
+    );
+  }
+}
+
+class _Body extends StatelessWidget {
+  const _Body();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 16),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(8),
+            child: ViewResult(),
+          ),
+          Column(
+            children: [
+              rowFourButtonsCalculator(),
+              rowFourButtonsCalculator(),
+              rowFourButtonsCalculator(),
+              rowThreeButtonsCalculator(),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget rowFourButtonsCalculator() {
+    return const Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Btn(),
+        Btn(),
+        Btn(),
+        Btn(),
+      ],
+    );
+  }
+
+  Widget rowThreeButtonsCalculator() {
+    return const Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Btn(width: 156),
+        Btn(),
+        Btn(),
+      ],
+    );
+  }
+}
+
+
+//TODO: Elinimar este codigo hacia abajo, y reemplazar el el boton
+class Btn extends StatelessWidget {
+  final double width;
+  const Btn({
+    super.key,
+    this.width = 72,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 12),
+      child: Container(
+        width: width,
+        height: 72,
+        decoration: BoxDecoration(
+            color: Colors.red, borderRadius: BorderRadius.circular(36)),
+      ),
+    );
+  }
+}
+
+class ViewResult extends StatelessWidget {
+  const ViewResult({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      '0',
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: 120,
+        fontWeight: FontWeight.w300,
       ),
     );
   }
