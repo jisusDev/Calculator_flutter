36f946b1e3959e13d6008f7d7c149149d78ee284
Francisco Colmenarez <156324130+Francisco-V98@users.noreply.github.com>
2024-03-13 14:56:37 -0400

    Logic screen app calculator (#9)
    * Change Container to SizedBox and overlayColor
    
    * add parameter onPressed
    
    * create first logic of calculator screen
    
    * remove sizedbox, remove ClipRRect, new materialProperty<size>
    
    ---------
    
    Co-authored-by: jesusDev <jesusdev2407@gmail.com>

--- lib/screens/calculator_screen/calculator_screen.dart
+++ lib/screens/calculator_screen/calculator_screen.dart
@@ -15,8 +15,74 @@ class CalculatorScreen extends StatelessWidget {
   }
 }
 
-class _Body extends StatelessWidget {
+class _Body extends StatefulWidget {
   const _Body();
+
+  @override
+  State<_Body> createState() => _BodyState();
+}
+
+class _BodyState extends State<_Body> {
+  String resultValue = '0';
+  String firstValue = '';
+  String secondValue = '';
+  double Function(double, double)? operation;
+  String highlightOperator = '';
+
+  void compute() {
+    setState(() {
+      resultValue = operation! 
+          .call(
+            double.parse(firstValue.replaceAll(',', '.')),
+            double.parse(secondValue.replaceAll(',', '.')),
+          ).toStringAsFixed(2);
+      firstValue = resultValue;
+      secondValue = '';
+      operation = null;
+    });
+  }
+
+  void addOperand(double Function(double, double) operation) {
+    if (firstValue.isEmpty) {
+      return;
+    }
+    if (secondValue.isNotEmpty) {
+      compute();
+    }
+    setState(() {
+      this.operation = operation;
+    });
+  }
+
+  void addNumber(String input) {
+    highlightOperator = '';
+    if (operation == null) {
+      setState(() {
+        firstValue += input; 
+        resultValue = firstValue;
+      });
+    } else {
+      setState(() {
+        secondValue += input;
+        resultValue = secondValue;
+      });
+    }
+  }
+
+  void reset() {
+    setState(() {
+      resultValue = '0';
+      firstValue = '';
+      secondValue = '';
+      operation = null;
+    });
+  }
+
+  // void changeColorOperator(){
+  //   if (condition) {
+      
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -26,9 +92,9 @@ class _Body extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.end,
         crossAxisAlignment: CrossAxisAlignment.end,
         children: [
-          const Padding(
-            padding: EdgeInsets.all(8),
-            child: ViewResult(value: '0'),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: ViewResult(value: resultValue),
           ),
           Column(
             children: [
@@ -45,139 +111,173 @@ class _Body extends StatelessWidget {
   }
 
   Widget firstRowCalculatorButtons() {
-    return const Row(
+    return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         CalculatorButton(
           text: 'C',
-          backgroundColor: Color.fromRGBO(199, 199, 204, 1),
+          backgroundColor: const Color.fromRGBO(199, 199, 204, 1),
           textColor: Colors.black,
+          onPressed: () {
+            reset();
+          },
         ),
         CalculatorButton(
           text: '±',
-          backgroundColor: Color.fromRGBO(199, 199, 204, 1),
+          backgroundColor: const Color.fromRGBO(199, 199, 204, 1),
           textColor: Colors.black,
+          onPressed: () {},
         ),
         CalculatorButton(
           text: '%',
-          backgroundColor: Color.fromRGBO(199, 199, 204, 1),
+          backgroundColor: const Color.fromRGBO(199, 199, 204, 1),
           textColor: Colors.black,
+          onPressed: () {},
         ),
         CalculatorButton(
           text: '÷',
-          backgroundColor: Color.fromRGBO(255, 149, 0, 1),
+          backgroundColor: const Color.fromRGBO(255, 149, 0, 1),
           textColor: Colors.white,
+          onPressed: () {
+            addOperand((p0, p1) => p0 / p1);
+            setState(() => highlightOperator = '÷');
+          },
         ),
       ],
     );
   }
 
   Widget secondRowCalculatorButtons() {
-    return const Row(
+    return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         CalculatorButton(
           text: '7',
-          backgroundColor: Color.fromARGB(255, 51,51,51),
+          backgroundColor: const Color.fromARGB(255, 51, 51, 51),
           textColor: Colors.white,
+          onPressed: () => addNumber('7'),
         ),
         CalculatorButton(
           text: '8',
-          backgroundColor: Color.fromARGB(255, 51,51,51),
+          backgroundColor: const Color.fromARGB(255, 51, 51, 51),
           textColor: Colors.white,
+          onPressed: () => addNumber('8'),
         ),
         CalculatorButton(
           text: '9',
-          backgroundColor: Color.fromARGB(255, 51,51,51),
+          backgroundColor: const Color.fromARGB(255, 51, 51, 51),
           textColor: Colors.white,
+          onPressed: () => addNumber('9'),
         ),
         CalculatorButton(
           text: '×',
-          backgroundColor: Color.fromRGBO(255, 149, 0, 1),
+          backgroundColor: const Color.fromRGBO(255, 149, 0, 1),
           textColor: Colors.white,
+          onPressed: () {
+            addOperand((p0, p1) => p0 * p1);
+            setState(() => highlightOperator = '×');
+          },
         ),
       ],
     );
   }
 
   Widget thirdRowCalculatorButtons() {
-    return const Row(
+    return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         CalculatorButton(
           text: '4',
-          backgroundColor: Color.fromARGB(255, 51,51,51),
+          backgroundColor: const Color.fromARGB(255, 51, 51, 51),
           textColor: Colors.white,
+          onPressed: () => addNumber('4'),
         ),
         CalculatorButton(
           text: '5',
-          backgroundColor: Color.fromARGB(255, 51,51,51),
+          backgroundColor: const Color.fromARGB(255, 51, 51, 51),
           textColor: Colors.white,
+          onPressed: () => addNumber('5'),
         ),
         CalculatorButton(
           text: '6',
-          backgroundColor: Color.fromARGB(255, 51,51,51),
+          backgroundColor: const Color.fromARGB(255, 51, 51, 51),
           textColor: Colors.white,
+          onPressed: () => addNumber('6'),
         ),
         CalculatorButton(
           text: '-',
-          backgroundColor: Color.fromRGBO(255, 149, 0, 1),
+          backgroundColor: const Color.fromRGBO(255, 149, 0, 1),
           textColor: Colors.white,
+          onPressed: () {
+            addOperand((p0, p1) => p0 - p1);
+            setState(() => highlightOperator = '-');
+          },
         ),
       ],
     );
   }
 
-    Widget fourthRowCalculatorButtons() {
-    return const Row(
+  Widget fourthRowCalculatorButtons() {
+    return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         CalculatorButton(
           text: '1',
-          backgroundColor: Color.fromARGB(255, 51,51,51),
+          backgroundColor: const Color.fromARGB(255, 51, 51, 51),
           textColor: Colors.white,
+          onPressed: () => addNumber('1'),
         ),
         CalculatorButton(
           text: '2',
-          backgroundColor: Color.fromARGB(255, 51,51,51),
+          backgroundColor: const Color.fromARGB(255, 51, 51, 51),
           textColor: Colors.white,
+          onPressed: () => addNumber('2'),
         ),
         CalculatorButton(
           text: '3',
-          backgroundColor: Color.fromARGB(255, 51,51,51),
+          backgroundColor: const Color.fromARGB(255, 51, 51, 51),
           textColor: Colors.white,
+          onPressed: () => addNumber('3'),
         ),
         CalculatorButton(
           text: '+',
-          backgroundColor: Color.fromRGBO(255, 149, 0, 1),
+          backgroundColor: const Color.fromRGBO(255, 149, 0, 1),
           textColor: Colors.white,
+          onPressed: () {
+            addOperand((p0, p1) => p0 + p1);
+            setState(() => highlightOperator = '+');
+          },
         ),
       ],
     );
   }
 
   Widget fivethRowCalculatorButtons() {
-    return const Row(
+    return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         CalculatorButton(
           width: 156,
           text: '0',
-          backgroundColor: Color.fromARGB(255, 51,51,51),
+          backgroundColor: const Color.fromARGB(255, 51, 51, 51),
           textColor: Colors.white,
+          onPressed: () => addNumber('0'),
         ),
         CalculatorButton(
           text: ',',
-          backgroundColor: Color.fromARGB(255, 51,51,51),
+          backgroundColor: const Color.fromARGB(255, 51, 51, 51),
           textColor: Colors.white,
+          onPressed: () => addNumber(','),
         ),
         CalculatorButton(
           text: '=',
-          backgroundColor: Color.fromRGBO(255, 149, 0, 1),
+          backgroundColor: const Color.fromRGBO(255, 149, 0, 1),
           textColor: Colors.white,
+          onPressed: () {
+            compute();
+          },
         ),
       ],
     );
   }
 }
-
